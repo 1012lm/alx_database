4ad96cd8560a0e1e6a0d516d98d9cccf3fd14280
1012lm <lm1012smile@gmail.com>
2023-08-16 12:24:50 -0800

    create

---
+++ SQL_introduction/1-create_database_if_missing.sql
@@ -0,0 +1,10 @@
+-- Script: 1-create_database_if_missing.sql
+-- Description: Creates the database hbtn_0c_0 if it does not already exist
+
+-- Check if the database hbtn_0c_0 exists
+
+CREATE DATABASE IF NOT EXISTS hbtn_0c_0;
+
+-- Switch to the hbtn_0c_0 database
+
+USE hbtn_0c_0;
