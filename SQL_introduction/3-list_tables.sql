3e8b0bea553cf5dfb77c894a873029e5a0385953
1012lm <lm1012smile@gmail.com>
2023-08-16 12:46:38 -0800

    list table

--- SQL_introduction/3-list_tables.sql
+++ SQL_introduction/3-list_tables.sql
@@ -2,11 +2,10 @@
 -- Description: Lists all tables of a specific database in the MySQL server
 
 -- Set the database name as a variable
-
 SET @dbname = 'hbtn_test_db_0';
 
 -- Retrieve the list of tables in the specified database
-
 SELECT TABLE_NAME
 FROM information_schema.TABLES
-WHERE TABLE_SCHEMA = @dbname;
+WHERE TABLE_SCHEMA = @dbname
+AND TABLE_TYPE = 'BASE TABLE';
