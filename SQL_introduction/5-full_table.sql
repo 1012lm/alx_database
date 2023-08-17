8a9360427321e7068709fb5a7b7b7a9a65c3d823
1012lm <lm1012smile@gmail.com>
2023-08-17 00:40:16 -0800

    full table

--- SQL_introduction/5-full_table.sql
+++ SQL_introduction/5-full_table.sql
@@ -9,7 +9,7 @@ SET @tablename = 'first_table';
 
 -- Retrieve the table creation statement from the information_schema.TABLES view
 SET @table_creation = (
-    SELECT CONCAT('CREATE TABLE `', TABLE_NAME, '` (', GROUP_CONCAT(COLUMN_NAME, ' ', COLUMN_TYPE, COLUMN_NULLABLE, COLUMN_DEFAULT, SEPARATOR ','), ') ', TABLE_ENGINE, ' DEFAULT CHARSET=', TABLE_COLLATION) AS statement
+    SELECT CONCAT('CREATE TABLE `', TABLE_NAME, '` (', GROUP_CONCAT(CONCAT('`', COLUMN_NAME, '` ', COLUMN_TYPE, COLUMN_NULLABLE, COLUMN_DEFAULT)), ', PRIMARY KEY (`id`)) ENGINE=', TABLE_ENGINE, ' DEFAULT CHARSET=', TABLE_COLLATION) AS statement
     FROM information_schema.COLUMNS
     WHERE TABLE_SCHEMA = @dbname
     AND TABLE_NAME = @tablename
