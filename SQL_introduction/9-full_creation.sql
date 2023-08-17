b1d172056b61882bc88d6c2c9fb66ee78761e5d5
1012lm <lm1012smile@gmail.com>
2023-08-16 23:57:41 -0800

    table creation

--- SQL_introduction/9-full_creation.sql
+++ SQL_introduction/9-full_creation.sql
@@ -1,6 +1,9 @@
 -- Script: 9-full_creation.sql
 -- Description: Creates the table second_table and inserts multiple rows
 
+-- Check if table exists
+SELECT COUNT(*) FROM information_schema.tables WHERE table_schema = 'hbtn_0c_0' AND table_name = 'second_table';
+
 -- Create table if it doesn't exist
 CREATE TABLE IF NOT EXISTS hbtn_0c_0.second_table (
     id INT,
@@ -13,3 +16,6 @@ INSERT INTO hbtn_0c_0.second_table (id, name, score) VALUES (1, 'John', 10);
 INSERT INTO hbtn_0c_0.second_table (id, name, score) VALUES (2, 'Alex', 3);
 INSERT INTO hbtn_0c_0.second_table (id, name, score) VALUES (3, 'Bob', 14);
 INSERT INTO hbtn_0c_0.second_table (id, name, score) VALUES (4, 'George', 8);
+
+-- Display all rows
+SELECT * FROM hbtn_0c_0.second_table;
