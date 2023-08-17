0562f1ed7b7d1fb48f61f2c5fa605d31bf6013d9
1012lm <lm1012smile@gmail.com>
2023-08-17 00:23:09 -0800

    list table

--- SQL_introduction/3-list_tables.sql
+++ SQL_introduction/3-list_tables.sql
@@ -7,4 +7,32 @@ SET @dbname = 'mysql';
 -- Retrieve the list of tables in the specified database
 SELECT TABLE_NAME
 FROM information_schema.TABLES
-WHERE TABLE_SCHEMA = @dbname;
+WHERE TABLE_SCHEMA = @dbname;-- Script: main_0.sql
+-- Description: Creates a new database with three tables and lists the tables
+
+-- Create the new database
+CREATE DATABASE IF NOT EXISTS hbtn_test_db_0;
+
+-- Use the new database
+USE hbtn_test_db_0;
+
+-- Create three tables
+CREATE TABLE IF NOT EXISTS holbteron_0 (
+    id INT,
+    name VARCHAR(256)
+);
+
+CREATE TABLE IF NOT EXISTS holbteron_1 (
+    id INT,
+    name VARCHAR(256)
+);
+
+CREATE TABLE IF NOT EXISTS holbteron_2 (
+    id INT,
+    name VARCHAR(256)
+);
+
+-- List the tables in the database
+SELECT TABLE_NAME
+FROM information_schema.TABLES
+WHERE TABLE_SCHEMA = 'hbtn_test_db_0';
