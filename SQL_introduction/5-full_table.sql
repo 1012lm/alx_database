bd79e0e4b8c1a0c50d430bf9aed26c0449489d5b
1012lm <lm1012smile@gmail.com>
2023-08-17 00:43:25 -0800

    full table

--- SQL_introduction/5-full_table.sql
+++ SQL_introduction/5-full_table.sql
@@ -9,12 +9,12 @@ SET @tablename = 'first_table';
 
 -- Retrieve the table creation statement from the information_schema.TABLES view
 SET @table_creation = (
-    SELECT CONCAT('CREATE TABLE `', TABLE_NAME, '` (', GROUP_CONCAT(CONCAT('`', COLUMN_NAME, '` ', COLUMN_TYPE, COLUMN_NULLABLE, COLUMN_DEFAULT)), ', PRIMARY KEY (`id`)) ENGINE=', TABLE_ENGINE, ' DEFAULT CHARSET=', TABLE_COLLATION) AS statement
+    SELECT CONCAT('CREATE TABLE `', TABLE_NAME, '` (\n', GROUP_CONCAT(COLUMN_NAME, ' ', COLUMN_TYPE, COLUMN_NULLABLE, COLUMN_DEFAULT SEPARATOR ',\n'), '\n) ENGINE=', TABLE_ENGINE, ' DEFAULT CHARSET=', CHARACTER_SET_NAME)
     FROM information_schema.COLUMNS
     WHERE TABLE_SCHEMA = @dbname
     AND TABLE_NAME = @tablename
-    GROUP BY TABLE_NAME, TABLE_ENGINE, TABLE_COLLATION
+    GROUP BY TABLE_NAME, TABLE_ENGINE, CHARACTER_SET_NAME
 );
 
 -- Print the full description of the table
-SELECT CONCAT(@tablename, @table_creation) AS Description;
+SELECT CONCAT(@tablename, '\n', @table_creation) AS Description;
