e597fc0f878d2b8c8116140060d4dc2302d6ec0c
1012lm <lm1012smile@gmail.com>
2023-08-17 01:45:45 -0800

    unique Id

--- SQL_more_queries/5-unique_id.sql
+++ SQL_more_queries/5-unique_id.sql
@@ -1,11 +1,9 @@
 -- Script: 5-unique_id.sql
--- Description: Creates the table unique_id with id and name columns
-
-USE `hbtn_0d_2`;
+-- Description: Creates the unique_id table
 
 -- Create table unique_id if it doesn't exist
 CREATE TABLE IF NOT EXISTS `unique_id` (
-  `id` INT DEFAULT 1,
+  `id` INT NOT NULL DEFAULT 1,
   `name` VARCHAR(256),
-  UNIQUE (`id`)
+  UNIQUE KEY `id` (`id`)
 );
