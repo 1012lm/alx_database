6b484d647379ffea563498b6af3416992e9f0989
1012lm <lm1012smile@gmail.com>
2023-08-17 00:03:10 -0800

    table

--- SQL_introduction/9-full_creation.sql
+++ SQL_introduction/9-full_creation.sql
@@ -1,18 +1,18 @@
--- Script: 9-full_creation.sql
--- Description: Creates the table second_table and inserts multiple rows
+#!/bin/bash
 
--- Create table if it doesn't exist
-CREATE TABLE IF NOT EXISTS hbtn_0c_0.second_table (
+# Script: 9-full_creation.sh
+# Description: Creates the table second_table and inserts multiple rows
+
+DB_NAME="$1"
+
+# Create table if it doesn't exist
+echo "CREATE TABLE IF NOT EXISTS ${DB_NAME}.second_table (
     id INT,
     name VARCHAR(256),
     score INT
-);
+);" | mysql -h localhost -u root -p
 
--- Insert multiple rows
-INSERT IGNORE INTO hbtn_0c_0.second_table (id, name, score) VALUES (1, 'John', 10);
-INSERT IGNORE INTO hbtn_0c_0.second_table (id, name, score) VALUES (2, 'Alex', 3);
-INSERT IGNORE INTO hbtn_0c_0.second_table (id, name, score) VALUES (3, 'Bob', 14);
-INSERT IGNORE INTO hbtn_0c_0.second_table (id, name, score) VALUES (4, 'George', 8);
+# Insert multiple rows
+echo "INSERT INTO ${DB_NAME}.second_table (id, name, score) VALUES (1, 'John', 10), (2, 'Alex', 3), (3, 'Bob', 14), (4, 'George', 8);" | mysql -h localhost -u root -p
 
--- Display all rows
-SELECT * FROM hbtn_0c_0.second_table;
+echo "Records inserted successfully."
