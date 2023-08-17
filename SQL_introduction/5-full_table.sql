16f447bea71aa54487f22f81b4917d879bd64960
1012lm <lm1012smile@gmail.com>
2023-08-17 02:27:07 -0800

    full table

--- SQL_introduction/5-full_table.sql
+++ SQL_introduction/5-full_table.sql
@@ -1,11 +1,8 @@
--- Script: 5-full_table.sql
+-- Script: main_0.sql
 -- Description: Prints the full description of the table first_table from the hbtn_0c_0 database
 
 -- Set the database name
 USE hbtn_0c_0;
 
 -- Get the table schema information
-SELECT TABLE_NAME AS 'Table', CREATE_TABLE
-FROM INFORMATION_SCHEMA.TABLES
-WHERE TABLE_SCHEMA = 'hbtn_0c_0'
-  AND TABLE_NAME = 'first_table';
+SHOW CREATE TABLE first_table;
