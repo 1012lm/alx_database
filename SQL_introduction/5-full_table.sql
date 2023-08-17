0915371719c884581f2c167c689ed755d650c66b
1012lm <lm1012smile@gmail.com>
2023-08-17 02:25:00 -0800

    full table

--- SQL_introduction/5-full_table.sql
+++ SQL_introduction/5-full_table.sql
@@ -1,20 +1,11 @@
 -- Script: 5-full_table.sql
--- Description: Prints the full description of the table first_table
+-- Description: Prints the full description of the table first_table from the hbtn_0c_0 database
 
--- Set the database name as a variable
-SET @dbname = 'hbtn_0c_0';
+-- Set the database name
+USE hbtn_0c_0;
 
--- Set the table name as a variable
-SET @tablename = 'first_table';
-
--- Retrieve the table creation statement from the information_schema.TABLES view
-SET @table_creation = (
-    SELECT CONCAT('CREATE TABLE `', TABLE_NAME, '` (\n', GROUP_CONCAT(COLUMN_NAME, ' ', COLUMN_TYPE, COLUMN_NULLABLE, COLUMN_DEFAULT SEPARATOR ',\n'), '\n) ENGINE=', TABLE_ENGINE, ' DEFAULT CHARSET=', CHARACTER_SET_NAME)
-    FROM information_schema.COLUMNS
-    WHERE TABLE_SCHEMA = @dbname
-    AND TABLE_NAME = @tablename
-    GROUP BY TABLE_NAME, TABLE_ENGINE, CHARACTER_SET_NAME
-);
-
--- Print the full description of the table
-SELECT CONCAT(@tablename, '\n', @table_creation) AS Description;
+-- Get the table schema information
+SELECT TABLE_NAME AS 'Table', CREATE_TABLE
+FROM INFORMATION_SCHEMA.TABLES
+WHERE TABLE_SCHEMA = 'hbtn_0c_0'
+  AND TABLE_NAME = 'first_table';
