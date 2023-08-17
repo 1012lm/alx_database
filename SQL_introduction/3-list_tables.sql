9314949029a4b40df90fa708d4ebc2afdee653f1
1012lm <lm1012smile@gmail.com>
2023-08-17 00:32:14 -0800

    list table

--- SQL_introduction/3-list_tables.sql
+++ SQL_introduction/3-list_tables.sql
@@ -7,28 +7,6 @@ CREATE DATABASE IF NOT EXISTS hbtn_test_db_1;
 -- Use the new database
 USE hbtn_test_db_1;
 
-
--- Create three tables
-CREATE TABLE IF NOT EXISTS holbteron_0 (
-    id INT,
-    name VARCHAR(256)
-);
-
-CREATE TABLE IF NOT EXISTS holbteron_1 (
-    id INT,
-    name VARCHAR(256)
-);
-
-CREATE TABLE IF NOT EXISTS holbteron_2 (
-    id INT,
-    name VARCHAR(256)
-);
-
--- List the tables in the database
-SHOW TABLES;
-
-
-
 -- List the tables in the database
 SELECT TABLE_NAME
 FROM information_schema.TABLES
