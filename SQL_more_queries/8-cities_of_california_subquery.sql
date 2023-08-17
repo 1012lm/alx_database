b4cc5fb4f6b3ae6b18c239ca5f2824d7d81c39fc
1012lm <lm1012smile@gmail.com>
2023-08-17 01:49:01 -0800

    california

--- SQL_more_queries/8-cities_of_california_subquery.sql
+++ SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,10 +1,8 @@
 -- Script: 8-cities_of_california_subquery.sql
--- Description: Lists all the cities of California from the hbtn_0d_usa database
+-- Description: Lists all cities of California
 
-USE `hbtn_0d_usa`;
+-- Find the state_id for California
+SELECT id INTO @california_id FROM states WHERE name = 'California';
 
--- Get the state_id of California
-SET @state_id := (SELECT id FROM states WHERE name = 'California');
-
--- List all the cities of California
-SELECT * FROM cities WHERE state_id = @state_id ORDER BY id ASC;
+-- List all cities of California
+SELECT * FROM cities WHERE state_id = @california_id ORDER BY id ASC;
