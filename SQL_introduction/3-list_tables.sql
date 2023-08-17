450afd12f21cc005988f4f954a68e11106233cf7
1012lm <lm1012smile@gmail.com>
2023-08-17 02:11:52 -0800

    list table

--- SQL_introduction/3-list_tables.sql
+++ SQL_introduction/3-list_tables.sql
@@ -1,13 +1,6 @@
--- Script: main_1.sql
--- Description: Creates a new database without tables and lists the tables
+-- Script: 3-list_tables.sql
+-- Description: Lists all tables in a specified database
 
--- Create the new database
-CREATE DATABASE IF NOT EXISTS hbtn_test_db_1;
-
--- Use the new database
-USE hbtn_test_db_1;
-
--- List the tables in the database
-SELECT TABLE_NAME
-FROM information_schema.TABLES
-WHERE TABLE_SCHEMA = 'hbtn_test_db_1';
+SELECT TABLE_NAME AS Tables_in_database
+FROM INFORMATION_SCHEMA.TABLES
+WHERE TABLE_SCHEMA = '<database_name>';
