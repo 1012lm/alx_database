63166dab9363318b924782d399e675761ce6968d
1012lm <lm1012smile@gmail.com>
2023-08-17 01:16:19 -0800

    force name

--- SQL_more_queries/3-force_name.sql
+++ SQL_more_queries/3-force_name.sql
@@ -1,11 +1,9 @@
 -- Script: 3-force_name.sql
--- Description: Creates the table force_name with id and name columns
-
-USE `hbtn_0d_2`;
+-- Description: Creates the force_name table
 
 -- Create table force_name if it doesn't exist
 CREATE TABLE IF NOT EXISTS `force_name` (
-  `id` INT,
+  `id` INT NOT NULL,
   `name` VARCHAR(256) NOT NULL,
   PRIMARY KEY (`id`)
 );
