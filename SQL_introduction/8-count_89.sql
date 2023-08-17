1e9ff87364e1cdec6689b71241c439d127d10f25
1012lm <lm1012smile@gmail.com>
2023-08-16 23:53:19 -0800

    count

---
+++ SQL_introduction/8-count_89.sql
@@ -0,0 +1,5 @@
+-- Script: 8-count_89.sql
+-- Description: Displays the number of records with id = 89 in the table first_table
+
+-- Count the number of records
+SELECT COUNT(*) FROM first_table WHERE id = 89;
