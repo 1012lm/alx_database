b09d60e76879a701d3fefd5f983a6e27f8c2800f
1012lm <lm1012smile@gmail.com>
2023-08-17 02:23:12 -0800

    list table

--- SQL_introduction/3-list_tables.sql
+++ SQL_introduction/3-list_tables.sql
@@ -1,18 +1,22 @@
--- Script: main_1.sql
--- Description: Creates a new database without any tables
+-- Script: main_0.sql
+-- Description: Creates a new database with three tables and lists the tables
 
 -- Create the database if it doesn't exist
-CREATE DATABASE IF NOT EXISTS hbtn_test_db_1;
+CREATE DATABASE IF NOT EXISTS hbtn_test_db_0;
 
 -- Switch to the newly created database
-USE hbtn_test_db_1;
+USE hbtn_test_db_0;
 
--- List the tables in the database (should be empty)
-SELECT 'No tables found' AS Tables_in_hbtn_test_db_1
-FROM DUAL
-WHERE NOT EXISTS (
-  SELECT 1
-  FROM INFORMATION_SCHEMA.TABLES
-  WHERE TABLE_SCHEMA = 'hbtn_test_db_1'
-  LIMIT 1
-);
+-- Create the first table if it doesn't exist
+CREATE TABLE IF NOT EXISTS holbteron_0 (id INT);
+
+-- Create the second table if it doesn't exist
+CREATE TABLE IF NOT EXISTS holbteron_1 (id INT);
+
+-- Create the third table if it doesn't exist
+CREATE TABLE IF NOT EXISTS holbteron_2 (id INT);
+
+-- List the tables in the database
+SELECT TABLE_NAME AS Tables_in_hbtn_test_db_0
+FROM INFORMATION_SCHEMA.TABLES
+WHERE TABLE_SCHEMA = 'hbtn_test_db_0';
