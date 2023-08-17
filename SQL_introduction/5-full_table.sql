cbe02373063341cdbd00f382bc1003c3ad95554f
1012lm <lm1012smile@gmail.com>
2023-08-16 23:37:24 -0800

    full table

--- SQL_introduction/5-full_table.sql
+++ SQL_introduction/5-full_table.sql
@@ -4,6 +4,7 @@
 -- Set the database name and table name as variables
 -- Create database and table
 
+
 DROP DATABASE IF EXISTS hbtn_test_db_5;
 CREATE DATABASE IF NOT EXISTS hbtn_test_db_5;
 USE hbtn_test_db_5;
@@ -13,4 +14,4 @@ CREATE TABLE IF NOT EXISTS first_table (
     c CHAR(1),
     created_at DATE,
     PRIMARY KEY (id)
-);
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
