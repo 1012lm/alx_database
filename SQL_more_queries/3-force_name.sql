00b734303c0cec6e6d33773e484d0f5dc3664aab
1012lm <lm1012smile@gmail.com>
2023-08-17 01:23:56 -0800

    force

--- SQL_more_queries/3-force_name.sql
+++ SQL_more_queries/3-force_name.sql
@@ -1,13 +1,10 @@
 -- Script: 3-force_name.sql
--- Description: Creates the force_name table and inserts data
+-- Description: Creates the force_name table if it doesn't exist and inserts data
 
--- Drop the force_name table if it exists
-DROP TABLE IF EXISTS `force_name`;
-
--- Create table force_name
-CREATE TABLE `force_name` (
+-- Create table force_name if it doesn't exist
+CREATE TABLE IF NOT EXISTS `force_name` (
   `id` INT NOT NULL,
-  `name` VARCHAR(256) NOT NULL,
+  `name` VARCHAR(256),
   PRIMARY KEY (`id`)
 );
 
@@ -18,3 +15,6 @@ INSERT INTO `force_name` (`id`, `name`) VALUES
   (3, 'Holberton'),
   (4, 'School'),
   (5, 'C is fun');
+
+-- Attempt an insert without a name
+INSERT INTO `force_name` (`id`) VALUES (6);
