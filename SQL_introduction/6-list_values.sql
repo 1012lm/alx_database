f779cac68e420b7f82aeeef3c7abddaa260b1f3b
1012lm <lm1012smile@gmail.com>
2023-08-16 23:45:17 -0800

    list values

--- SQL_introduction/6-list_values.sql
+++ SQL_introduction/6-list_values.sql
@@ -1,9 +1,5 @@
 -- Script: 6-list_values.sql
 -- Description: Lists all rows of the table first_table
 
--- Set the database name and table name as variables
-SET @dbname = 'hbtn_0c_0';
-SET @tablename = 'first_table';
-
 -- Retrieve all rows from the table
-SELECT * FROM @dbname.@tablename;
+SELECT * FROM hbtn_0c_0.first_table;
