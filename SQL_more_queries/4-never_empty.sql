8d8806ea5473dc41ce1e432f76572fe698301985
1012lm <lm1012smile@gmail.com>
2023-08-17 01:41:30 -0800

    never empty

--- SQL_more_queries/4-never_empty.sql
+++ SQL_more_queries/4-never_empty.sql
@@ -1,10 +1,8 @@
 -- Script: 4-never_empty.sql
--- Description: Creates the table id_not_null with id and name columns
-
-USE `hbtn_0d_2`;
+-- Description: Creates the id_not_null table
 
 -- Create table id_not_null if it doesn't exist
 CREATE TABLE IF NOT EXISTS `id_not_null` (
-  `id` INT DEFAULT 1,
+  `id` INT NOT NULL DEFAULT 1,
   `name` VARCHAR(256)
 );
