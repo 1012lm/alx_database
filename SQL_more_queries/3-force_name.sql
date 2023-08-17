8d6ac62de2793daddf77e1533fd3b781f9cd358d
1012lm <lm1012smile@gmail.com>
2023-08-17 01:38:49 -0800

    force table

--- SQL_more_queries/3-force_name.sql
+++ SQL_more_queries/3-force_name.sql
@@ -4,16 +4,15 @@
 -- Create table force_name if it doesn't exist
 CREATE TABLE IF NOT EXISTS `force_name` (
   `id` INT NOT NULL,
-  `name` VARCHAR(256) NOT NULL
+  `name` VARCHAR(256) NOT NULL,
+  PRIMARY KEY (`id`)
 );
 
--- Add primary key to the force_name table
-ALTER TABLE `force_name` ADD PRIMARY KEY (`id`);
-
 -- Insert data into the force_name table
-INSERT IGNORE INTO `force_name` (`id`, `name`) VALUES
+INSERT INTO `force_name` (`id`, `name`) VALUES
   (1, 'Holberton School'),
-  (1, 'Python is cool'),
-  (2, 'Holberton'),
-  (3, 'School'),
-  (4, 'C is fun');
+  (2, 'Python is cool'),
+  (3, 'Holberton'),
+  (4, 'School'),
+  (5, 'C is fun')
+  ON DUPLICATE KEY UPDATE `name` = VALUES(`name`);
