0544f7888fb6e6c1ff19617aaeb0b2085389629e
1012lm <lm1012smile@gmail.com>
2023-08-17 01:56:55 -0800

    california

--- SQL_more_queries/8-cities_of_california_subquery.sql
+++ SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,8 +1,8 @@
--- Script: 8-cities_of_california_subquery.sql
+-- Script: main_0_0.sql
 -- Description: Lists all cities of California
 
 -- Find the state_id for California
 SELECT id INTO @california_id FROM states WHERE name = 'California';
 
 -- List all cities of California
-SELECT * FROM cities WHERE state_id = @california_id ORDER BY id ASC;
+SELECT id, name FROM cities WHERE state_id = @california_id ORDER BY id ASC;
